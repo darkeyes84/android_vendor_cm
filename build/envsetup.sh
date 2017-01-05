660cecd0c73793092d0204d08d91fe529bd5ff83
Dark Eyes <dark_eyes_@hotmail.com>
2017-01-05 18:37:39 -0500

    Revert to cm naming to keep compatibility with opendelta
    Change-Id: Ia1efddf8fad8fe2c6dc309c9ef6554fb9382a0f8

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -81,7 +81,7 @@ function eat()
 {
     if [ "$OUT" ] ; then
         MODVERSION=$(get_build_var LINEAGE_VERSION)
-        ZIPFILE=lineage-$MODVERSION.zip
+        ZIPFILE=cm-$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
