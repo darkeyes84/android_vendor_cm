fc009dd62a04296c7801f23dc2284bc5be6dc189
Dark Eyes <dark_eyes_@hotmail.com>
2017-01-05 18:44:36 -0500

    Revert "Revert "cm: Revert the new boot animation generation process""
    This reverts commit 24a1cc95b67460edff372f1d01a3fbc74f485117.
    
    Change-Id: Ie7b4e6c2fb26869c3258000bad4c82f24f190b70
    
    # Conflicts:
    #	config/common.mk

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -2,3 +2,8 @@
 $(call inherit-product, vendor/cm/config/common_full.mk)
 
 PRODUCT_PACKAGES += TvSettings
+
+ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+    PRODUCT_COPY_FILES += \
+        vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+endif
