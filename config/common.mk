d6558691c24e72bfa89fe6cfbc1e39b71a910729
Dark Eyes <dark_eyes_@hotmail.com>
2017-01-22 21:28:58 -0500

    revert vendor: lineage boot animation
    Change-Id: Iff2087eb97aee5e586eecf3898f0066d99bc885c (reverted from commit e035b9ab41b5744859b5ca9eac3cafd3862b787d)

--- config/common.mk
+++ config/common.mk
@@ -88,8 +88,8 @@ include vendor/cm/config/cmsdk_common.mk
 endif
 
 # Bootanimation
-PRODUCT_PACKAGES += \
-    bootanimation.zip
+#PRODUCT_PACKAGES += \
+#    bootanimation.zip
 
 # OpenDelta
 PRODUCT_PACKAGES += \
