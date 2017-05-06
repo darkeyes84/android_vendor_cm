c776f36269aab83a59987453a264beee1cbc0679
Dark Eyes <dark_eyes_@hotmail.com>
2017-05-06 08:16:50 -0400

    Rename to darkrom
    Change-Id: I7512850630377fd4b7034d34c729bf3adf5decc1

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -81,7 +81,7 @@ function eat()
 {
     if [ "$OUT" ] ; then
         MODVERSION=$(get_build_var LINEAGE_VERSION)
-        ZIPFILE=cm-$MODVERSION.zip
+        ZIPFILE=darkrom-$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
